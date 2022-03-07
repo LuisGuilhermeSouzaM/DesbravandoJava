5df45a6810a969f53ad1745c05379e2b37d08f43
LuisGuilhermeMunhoz <mnomeluisguilherme@yahoo.com>
2022-03-07 10:17:39 -0300

    primeiro commit

---
+++ src/Livro.java
@@ -0,0 +1,30 @@
+public class Livro {
+    String nome;
+    String descricao;
+    double valor;
+    String isbn;
+    Autor autor;
+
+    boolean temAutor(){
+        return this.autor != null;
+    }
+    
+    void mostrarDetalhes() {
+        System.out.println("Mostrando detalhes do livro ");
+        System.out.println("Nome: " + nome);
+        System.out.println("Descricao: " + descricao);
+        System.out.println("Valor: " + valor);
+        System.out.println("ISBN: " + isbn);
+
+        if(this.temAutor()) { 
+            autor.mostrarDetalhes();
+        }
+        
+        System.out.println("--");
+    }
+
+    public void aplicaDescontoDe(double porcentagem){
+        this.valor -= this.valor * porcentagem;
+
+    }
+}
